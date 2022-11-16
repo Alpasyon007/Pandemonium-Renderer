1d6b83d958d8c174604f61ce1258ba265306a2d0
Alpasyon007 <alpasyon07@gmail.com>
2022-11-16 04:24:33 +0000

    Implemented Loading shaders from glsl files

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -5,6 +5,22 @@
 
 namespace Pandemonium {
 
+	Shader* Shader::Create(const std::string& filepath) {
+		switch(Renderer::GetAPI()) {
+			case RendererAPI::API::None: {
+				LOG_ERROR("RendererAPI::None is currently not supportetd!");
+				DEBUG_BREAK();
+				return nullptr;
+			}
+			case RendererAPI::API::OpenGL: {
+				return new OpenGLShader(filepath);
+			}
+		}
+
+		LOG_ERROR("Unknown Renderer API!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc) {
 		switch(Renderer::GetAPI()) {
 			case RendererAPI::API::None: {
